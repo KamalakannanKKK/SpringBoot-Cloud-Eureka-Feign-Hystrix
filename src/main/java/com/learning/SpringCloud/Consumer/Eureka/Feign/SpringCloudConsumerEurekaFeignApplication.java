48e80a4983b5598fee5c8d809f4f0102587fec3d
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-05-10 14:37:08 +0530

    Update SpringCloudConsumerEurekaFeignApplication.java

--- src/main/java/com/learning/SpringCloud/Consumer/Eureka/Feign/SpringCloudConsumerEurekaFeignApplication.java
+++ src/main/java/com/learning/SpringCloud/Consumer/Eureka/Feign/SpringCloudConsumerEurekaFeignApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @EnableFeignClients
 @SpringBootApplication
